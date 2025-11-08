0366593435d39c865d156894eef3680d1179fe74
Daniyal Ahmed <da9033431@gmail.com>
2025-11-08 22:07:16 +0000

    Auth Form Setup

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import AuthForm from '@/components/AuthForm.tsx'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <AuthForm type ="sign-up"/>
+    </div>
+  )
+}
+
+export default page
